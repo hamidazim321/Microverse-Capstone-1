73ca78b81b086f0a77852d2212c44fb8b69a8c78
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-06-25 23:25:42 +0500

    Add font-awesome links on About Page

--- About.js
+++ About.js
@@ -1,7 +1,10 @@
 import LoadMenu from './Modules/Mobile-Menu.js';
 
-if (window.innerWidth < 768) {
+
   window.addEventListener('load', () => {
     LoadMenu();
   });
-}
+
+  window.addEventListener('resize', () => {
+    LoadMenu();
+  });
